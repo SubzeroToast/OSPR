f8d7dba9120d5393a43b92761a444608c5aa5a6d
SubzeroToast <kruhkonja@gmail.com>
2025-09-09 14:56:15 +0200

    Update naloge2.py

--- naloge2.py
+++ naloge2.py
@@ -1,1 +1,7 @@
-
+operator = str(input("Vpisi * + - /"))
+stevilo1 = int(input("Vpisi stevilo1: "))
+stevilo2 = int(input("Vpisi stevilo2: "))
+seznam = [operator, stevilo1, stevilo2]
+print(seznam)
+def kalkulator(seznam):
+    
