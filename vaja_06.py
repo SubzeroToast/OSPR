a334c948c072f57e35e93c199d4a43aa31ba9344
SubzeroToast <kruhkonja@gmail.com>
2025-10-21 15:03:33 +0200

    Update vaja_06.py

--- vaja_06.py
+++ vaja_06.py
@@ -110,20 +110,62 @@ print(poisci_element([30, 25, 20, 15, 10, 5], 5))
 # Primer:
 # poisci_palindrome('mkdmadamprvlevelkdrnoonkdc') vrne['ada', 'level', 'eve', 'noon', 'oo')
 
+def poisci_palindrome(niz):
+    master_sez = []
+    for i in range(len(niz)):
+        sez = ""
+        for j in range(i,len(niz)):
+            sez+=niz[j]
+            if sez == sez[::-1] and len(sez)>1:
+                master_sez.append(sez)
+    return master_sez
+print(poisci_palindrome("mkdmadamprvlevelkdrnoonkdc"))
+
 # 9. Naloga
 # Napišite funkcijo odstrani_dvojnike(sez), ki vrne seznam z odstranjenimi ponovitvami elementov. Elemente iz seznama odstranite brez, da ustvarite nov seznam.
 # Primer:
 # odstrani_dvojike([3, 9, 'ab', 5, 'mmm', 0, 9, 'lda', 'mmm', 0]) vrne [3, 9, 'ab', 5, 'mmm', 0, 'lda']
+
+def odstrani_dvojnike(sez):
+    return list(set(sez))
+print(odstrani_dvojnike([3, 9, 'ab', 5, 'mmm', 0, 9, 'lda', 'mmm', 0]))
 
 # 10. Naloga
 # Napišite funkcijo podseznami_z_vsoto(seznam, cilj), ki poišče in vrne vse podsezname v seznamu, katerih vsota je enaka podanemu cilju.
 # Primer:
 # podseznami_z_vsoto([1, 2, 3, 4, 5], 9) vrne [[2, 3, 4], [4, 5]]
 
+def podseznami_z_vsoto(seznam, cilj):
+    master_sez = []
+    for i in range(len(seznam)):
+        sez = ""
+        for j in range(i,len(seznam)):
+            sez+=str(seznam[j])
+            if sestej_stevke(sez) == cilj:
+                master_sez.append(list(sez))
+    return master_sez
+
+def sestej_stevke(stevilo):
+    stevilo = int(stevilo)
+    rez = 0
+    while stevilo:
+        rez = rez + stevilo%10
+        stevilo //= 10
+    return rez
+
+print(podseznami_z_vsoto([1, 2, 3, 4, 5], 9))
+
 # 11. Naloga
 # Napišite funkcijo najpogostejsi_element(seznam), ki prejme seznam in poišče elemet, ki se v seznamu najpogosteje pojavlja. Če je v seznamu več elementov z enakim številom pojavitev, vrni tistega z najnižjo vrednostjo.
 # Primer:
 # najpogostejsi_element([1, 3, 2, 3, 2, 2, 4]) vrne 2
+
+import collections
+
+def najpogostejsi_element(seznam):
+    dickt = collections.Counter(seznam)
+    return max(dickt.values())
+print(najpogostejsi_element([1, 3, 2, 3, 2, 2, 4]))
 
 # 12. Naloga
 # Napišite funkcijo najdaljse_zaporeje(seznam), ki vrne najdaljše naraščajoče zaporedje elementov.
